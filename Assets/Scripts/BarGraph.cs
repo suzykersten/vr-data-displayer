480d8c5067edd92b2025dc4fff6b41a1970eac0f
Suzy Kersten <suzykersten72@outlook.com>
2019-04-21 22:03:05 -0500

    columns have tooltips now!

--- Assets/Scripts/BarGraph.cs
+++ Assets/Scripts/BarGraph.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BarGraph : MonoBehaviour
 {
     public GameObject model = null;
     public TextAsset text = null;
+
+    public GameObject GvrPointer = null;
 
     public int modelsPerRow;
     public float heightBetweenRows;
@@ -43,19 +46,85 @@ public class BarGraph : MonoBehaviour
             starty = y = this.transform.position.y;
             startz = z = this.transform.position.z;
 
+            int columnNumber = 0;
+
             foreach(DataPoint point in dataPoints)
             {
-                point.value = point.value / 9;
+                //holds all the data for this object
+                GameObject column = new GameObject();
+                column.name = "Col " + columnNumber++ + ": data-" + point.value ;
+                column.transform.parent = this.transform;
+                column.transform.position = new Vector3(x, y, z);
+
+                //generate a tooltip
+                if (GvrPointer != null)
+                {
+                    GameObject tooltip = new GameObject();
+                    TextMesh textMesh = tooltip.AddComponent<TextMesh>();
+
+                    textMesh.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+                    textMesh.GetComponent<Renderer>().material = textMesh.font.material;
+                    textMesh.color = Color.black;
+                    textMesh.text = point.name;
+
+                    tooltip.transform.parent = GvrPointer.transform;
+                    tooltip.transform.position = new Vector3(0, 0, 20);
+
+                    //make a script to toggle the visibility of the tooltip
+                    tooltip.AddComponent<toggleVisible>();
+                    tooltip.SetActive(false);
+
+
+                    //make box collider for tag output
+                    GameObject tag = new GameObject();
+                    tag.name = "col tag";
+                    tag.transform.parent = column.transform;
+                    tag.transform.position = new Vector3(x, y, z);
+
+                    tag.AddComponent<BoxCollider>();
+
+                    BoxCollider tagCollider = tag.GetComponent<BoxCollider>();
+
+                    Bounds modelBounds = model.GetComponent<MeshFilter>().sharedMesh.bounds;
+
+                    //size of the bar
+                    float tagColliderHeight = ((modelBounds.size.y * model.transform.localScale.y)) * point.value
+                        + heightBetweenRows / 2 * point.value
+                        - heightBetweenRows;
+
+                    tagCollider.size = new Vector3(modelBounds.size.x * model.transform.localScale.x,
+                            tagColliderHeight,
+                            modelBounds.size.z * model.transform.localScale.z);
+                    tagCollider.center = new Vector3(0, tagColliderHeight / 2.0f, 0);
+
+
+                    //make tooltip visible on gaze over
+                    EventTrigger tagTrigger =  tag.AddComponent<EventTrigger>();
+                    EventTrigger.Entry entry = new EventTrigger.Entry();
+                    entry.eventID = EventTriggerType.PointerEnter;
+                    entry.callback.AddListener((data) => { tooltip.GetComponent<toggleVisible>().makeVisible(); });
+                    tagTrigger.triggers.Add(entry);
+
+                    //and remove tooltip on gaze leave
+                    EventTrigger.Entry entry2 = new EventTrigger.Entry();
+                    entry2.eventID = EventTriggerType.PointerExit;
+                    entry2.callback.AddListener((data) => { tooltip.GetComponent<toggleVisible>().makeInvisible(); });
+                    tagTrigger.triggers.Add(entry2);
+
+                }
+                
+               
+               
                 int row = 0;
 
                 for (int i = 0; i < point.value; i++)
                 {
 
-
+                    //make the object for this increment of the dataset
                     GameObject instance = Instantiate(model);
                     instance.isStatic = true;
 
-                    instance.transform.parent = this.transform;
+                    instance.transform.parent = column.transform;
                     instance.transform.position = new Vector3(x, y, z);
 
                     x -= paddingInBars;
---
+++ Assets/Scripts/toggleVisible.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toggleVisible : MonoBehaviour
+{
+    public void makeInvisible()
+    {
+         this.gameObject.SetActive(false);
+    }
+
+    public void makeVisible()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
